a8bf7156b50cf6ed9add2ae151c06e611a327281
Dharma Raju <sdharmacse@gmail.com>
2022-06-23 22:48:38 +0530

    Update mountfileshare.ps1

--- mountfileshare.ps1
+++ mountfileshare.ps1
@@ -5,13 +5,10 @@
 [Parameter(Mandatory = $true)]$DriveLetter
 )
 
-
-
-
 $connectTestResult = Test-NetConnection -ComputerName "$StorageAccountName.file.core.windows.net" -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:`"$StorageAccountName.file.core.windows.net`" /user:`"localhost\$StorageAccountName`" /pass:$key"
+    cmd.exe /C "cmdkey /add:`"$($StorageAccountName).file.core.windows.net`" /user:`"localhost\$StorageAccountName`" /pass:$key"
     # Mount the drive
     New-PSDrive -Name $DriveLetter -PSProvider FileSystem -Root "\\$StorageAccountName.file.core.windows.net\$FileShareName" -Persist
 } else {

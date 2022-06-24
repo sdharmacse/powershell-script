f82e5d588df1f20ae1a19fd2ab68376678467f58
Dharma Raju <sdharmacse@gmail.com>
2022-06-24 11:21:53 +0530

    Update mountfileshare.ps1

--- mountfileshare.ps1
+++ mountfileshare.ps1
@@ -9,8 +9,9 @@ $connectTestResult = Test-NetConnection -ComputerName "$StorageAccountName.file.
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
     cmd.exe /C "cmdkey /add:`"$($StorageAccountName).file.core.windows.net`" /user:`"localhost\$StorageAccountName`" /pass:$key"
-    # Mount the drive
-    New-PSDrive -Name $DriveLetter -PSProvider FileSystem -Root "\\$StorageAccountName.file.core.windows.net\$FileShareName"
+    # Mount the drive -Persist 
+    New-PSDrive -Name $DriveLetter -PSProvider FileSystem -Root "\\$StorageAccountName.file.core.windows.net\$FileShareName\"  -Persist 
+
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 } 
